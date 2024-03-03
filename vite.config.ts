279a6482e85d634742572d19e123bad3369579ee
Saeed Hossen <appsaeed7@gmail.com>
2024-03-04 00:59:41 +0600

    make ready to deploy +10

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
-import react from '@vitejs/plugin-react-swc'
-import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
+
+import { loadEnv } from "vite";
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+//@ts-ignore
+const env = loadEnv("mock", process.cwd(), "");
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-
+  base: env.VITE_BASENAME || "./",
   server: {
     port: 3000
   }
