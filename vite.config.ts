c63a4861466b03ffda00002507993098bdd39e72
Saeed Hossen <appsaeed7@gmail.com>
2024-03-04 01:25:34 +0600

    make ready to deploy +12

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,6 @@
 import react from '@vitejs/plugin-react-swc';
+import { readFile, writeFile } from "fs/promises";
+import { resolve } from "path";
 import { defineConfig } from 'vite';
 
 import { loadEnv } from "vite";
@@ -13,5 +15,24 @@ export default defineConfig({
   base: env.VITE_BASENAME || "./",
   server: {
     port: 3000
-  }
+  },
+  build: {
+    rollupOptions: {
+      plugins: [
+        {
+          name: "404-html",
+          async writeBundle(options) {
+            const indexHtmlPath = resolve(options.dir || '', "index.html");
+            const buildIndexHtmlPath = resolve(options.dir || '', "404.html");
+            try {
+              const indexHtmlContent = await readFile(indexHtmlPath, "utf-8");
+              await writeFile(buildIndexHtmlPath, indexHtmlContent);
+            } catch (error) {
+              console.error("Error duplicating index.html:", error);
+            }
+          },
+        },
+      ],
+    },
+  },
 })
