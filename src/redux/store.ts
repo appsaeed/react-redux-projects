6b615dfc809afa1f01ca240244a2331556603138
Saeed Hossen <appsaeed7@gmail.com>
2024-03-06 00:37:52 +0600

    add todo application 6+

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,16 +1,11 @@
 import { applyMiddleware, legacy_createStore as createStore } from 'redux';
 import { saveShopeStorage } from '../pages/shop/redux/shopMiddleware';
-import { fetchTodos, saveTodoStorage } from '../pages/todos/redux/todoMiddleware';
+import { saveTodoStorage } from '../pages/todos/redux/todoMiddleware';
 import reducer from './reducer';
 
 
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 //@ts-ignore
-const store = createStore(reducer, applyMiddleware(saveShopeStorage, saveTodoStorage, fetchTodos));
-
-store.dispatch({
-    type: 'hidden-action',
-    payload: []
-})
+const store = createStore(reducer, applyMiddleware(saveShopeStorage, saveTodoStorage));
 
 export default store;
